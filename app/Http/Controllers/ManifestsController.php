5665d8025de97aabe78297273536014f8e944eb5
Emmanuel <ebube.okeke@hotmail.com>
2019-06-19 22:39:02 +0100

    Manifest Resource

---
+++ app/Http/Controllers/ManifestsController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Manifest;
+use Illuminate\Http\Request;
+
+class ManifestsController extends Controller
+{
+	public function index()
+	{
+		
+	}
+
+    public function store(Request $request)
+    {
+    	Manifest::create($this->validateRequest());
+    	return redirect('/manifest');
+    }
+
+    public function update(Request $request, $id)
+    {
+
+    	Manifest::find($id)->update($this->validateRequest());
+
+    	return redirect('/manifest');
+    }
+
+    public function destory(Manifest $manifest, $id)
+    {
+    	Manifest::find($id)->delete();
+
+    	return redirect('/manifest');
+    }
+
+
+    /**
+     * @return mixed
+    */
+    public function validateRequest()
+    {
+    	$validatedData = request()->validate([
+	        'name' => 'required',
+	        'gender' => 'required',
+	        'seat_number' => 'required',
+	        'phone' => 'required',
+	        'address' => 'required',
+    	]);
+
+    	return $validatedData;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('/manifest')->group(function(){
+	Route::get('/', 'ManifestsController@index');
+	Route::post('/', 'ManifestsController@store');
+	Route::put('/{id}', 'ManifestsController@update');
+	Route::delete('/{id}', 'ManifestsController@destory');
+});
